61a9e35fe1115f6ebf5421338cc418addb3b577f
berkay ersoy <ersoyberkay_fb@hotmail.com>
2021-05-09 01:20:23 +0300

    router service added

---
+++ src/app/services/router.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RouterService {
+
+  constructor(private router:Router) { }
+
+  refreshPage(){
+    let currentUrl = this.router.url;
+    this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
+      this.router.navigate([currentUrl]);
+      console.log(currentUrl);
+  });
+}
+}
