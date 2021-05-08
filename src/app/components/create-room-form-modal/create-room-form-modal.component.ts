3339930f39470a4c723955a3c949816a91c081cc
berkay ersoy <ersoyberkay_fb@hotmail.com>
2021-05-09 01:19:57 +0300

    create room form modal added

---
+++ src/app/components/create-room-form-modal/create-room-form-modal.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { RoomService } from 'src/app/services/room.service';
+import { RouterService } from 'src/app/services/router.service';
+
+@Component({
+  selector: 'app-create-room-form-modal',
+  templateUrl: './create-room-form-modal.component.html',
+  styleUrls: ['./create-room-form-modal.component.css'],
+})
+export class CreateRoomFormModalComponent implements OnInit {
+  roomForm: FormGroup;
+  clicked = false;
+
+  constructor(
+    public activeModal: NgbActiveModal,
+    private modalService: NgbModal,
+    private formBuilder: FormBuilder,
+    private roomService: RoomService,
+    private toastrService: ToastrService,
+    private routerService: RouterService
+  ) {}
+
+  ngOnInit(): void {
+    this.createRoomForm();
+  }
+
+  createRoomForm() {
+    this.roomForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      description: [''],
+    });
+  }
+
+  createRoom() {
+    this.clicked = true;
+    if (this.roomForm.valid) {
+      let room = Object.assign({}, this.roomForm.value);
+      this.roomService.add(room).subscribe(
+        (response) => {
+          this.toastrService.success(response.message);
+          this.activeModal.dismiss();
+          this.routerService.refreshPage();
+        },
+        (errorResponse) => {
+          this.toastrService.error(errorResponse.error.message);
+        }
+      );
+    } else {
+      this.toastrService.error('Eksik bilgi girdiniz!');
+    }
+  }
+}
