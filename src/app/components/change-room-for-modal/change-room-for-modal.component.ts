6eb5ccb45078b048c037ad414e8d9f455ddcb9f6
berkay ersoy <ersoyberkay_fb@hotmail.com>
2021-05-12 09:14:11 +0300

    update room modal added

---
+++ src/app/components/change-room-for-modal/change-room-for-modal.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { Room } from 'src/app/models/room';
+import { RoomService } from 'src/app/services/room.service';
+
+@Component({
+  selector: 'app-change-room-for-modal',
+  templateUrl: './change-room-for-modal.component.html',
+  styleUrls: ['./change-room-for-modal.component.css'],
+})
+export class ChangeRoomForModalComponent implements OnInit {
+  clicked = false;
+  roomForm: FormGroup;
+  room:Room;
+
+  constructor(
+    public activeModal: NgbActiveModal,
+    private formBuilder: FormBuilder,
+    private toastrService: ToastrService,
+    private roomService: RoomService
+  ) {}
+
+  ngOnInit(): void {
+    this.createRoomForm();
+  }
+
+  createRoomForm(){
+    this.roomForm = this.formBuilder.group({
+      name:[this.room.name,Validators.required],
+      description:[this.room.description]
+    });
+  }
+  updateRoom(){
+    if(this.roomForm.valid){
+      this.clicked=true;
+      let room = Object.assign({},this.roomForm.value,{id:this.room.id});
+      this.roomService.update(room).subscribe(response=>{
+        this.toastrService.success(response.message)
+        this.activeModal.dismiss();
+        this.room=room;
+      },responseError=>{
+        this.clicked=false;
+        this.toastrService.error(responseError.error.message)
+      });
+    }
+  }
+}
